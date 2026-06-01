ef5747bb0792ba8af5b1e5444905abd2759ff52f
MAnxo7 <miguel_anxorl_05@hotmail.es>
2026-06-01 11:41:51 +0200

    Versión solida con tuplas

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -12,7 +12,7 @@ def decode(list_bytes:list) -> str:
 
 def create_bpe_tokenization(text:str) -> None: # De momento ns que devuelve
     import re
-    init_vocab = [chr(i) for i in range(0,256)]
+    init_vocab = [chr(i) for i in range(0,255)]
     pieces = set(re.findall(r"([a-zA-Z']{2,}|[^a-zA-Z']{2,})",text)) # This takes 2+ length elements of puntuaction or words
     tokens : list = []
     rules = {}
@@ -24,23 +24,23 @@ def create_bpe_tokenization(text:str) -> None: # De momento ns que devuelve
     for i in range(0,300): # 1000 new tokens for example
 
         pair_count = dict()
-        for token in tokens:
-            for i in range(0,len(token)-1):
-                key = token[i] + '+' + token[i+1]
+        for token_word in tokens:
+            for i in range(0,len(token_word)-1):
+                key = (token_word[i],token_word[i+1])
                 value = pair_count.get(key)
                 pair_count[key] =  value + 1 if value is not None else 1
 
         max_count_element = max(pair_count, key=lambda value : pair_count.get(value)) #Get the most repeated key in the dictionary
         #print(max_count_element, pair_count.get(max_count_element))
 
-        rule_value = "".join(str(max_count_element).split('+'))
+        rule_value = max_count_element[0] + max_count_element[1]
         rules[max_count_element] = rule_value
         #print(rules)
 
         for token_word in tokens: # Cada palabra en la lista de palabras segmentada en tokens...
             i = 0
             while i < len(token_word) - 1:
-                key = token_word[i] + '+' + token_word[i+1]
+                key = (token_word[i],token_word[i+1])
                 value = rules.get(key)
                 if value is not None:
                     token_word[i] = value
@@ -48,5 +48,5 @@ def create_bpe_tokenization(text:str) -> None: # De momento ns que devuelve
                 i+=1
     new_vocab = ([token for token_word in tokens for token in token_word])
 
-    print(set(init_vocab).union(new_vocab))
+    print(pair_count)
     return set(init_vocab).union(new_vocab)
